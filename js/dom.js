e6f9ba120818c063bb4c5e50e45ce288d4ed9555
Gabriel Duarte <gblduarte99@gmail.com>
2022-09-11 04:15:28 -0300

    Desafios DOM hechos

--- js/dom.js
+++ js/dom.js
@@ -1,15 +1,85 @@
 const tabla = document.querySelector("#tabla");
-// debugger
+const inputId = document.querySelector("#ordenId");
+const inputNombre = document.querySelector("#ordenNombre");
+const inputPrecio = document.querySelector("#ordenPrecio")
+const inputFiltro = document.querySelector("#buscarProd");
 
-function cargarProductos() {
+cargarProductos(productos);
+
+inputId.addEventListener("click", ordenId);
+inputNombre.addEventListener("click", ordenNombre);
+inputPrecio.addEventListener("click", ordenPrecio);
+inputFiltro.addEventListener("input", filtroProductos);
+
+
+
+
+// FUNCIONES
+function cargarProductos(arr) {
     let filaTabla = "";
-    productos.forEach(producto => {
+    tabla.innerHTML = "";
+    arr.forEach(producto => {
         filaTabla = `<tr>
-        <td>${producto.id}</td>
-        <td>${producto.nombre}</td>
-        <td>$${producto.precio}</td>
-        </tr>`
-        tabla.innerHTML += filaTabla;
+                        <td>${producto.id}</td>
+                        <td>${producto.nombre}</td>
+                        <td>$${producto.precio}</td>
+                    </tr>`
+                    tabla.innerHTML += filaTabla;
     })
 }
-cargarProductos();
+
+function ordenId() {
+    productos.sort((a, b) => {
+        if (a.id > b.id) {
+            return 1;
+        }
+        if (a.id < b.id) {
+            return -1;
+        }
+        return 0;
+    })
+    cargarProductos(productos);
+}
+
+function ordenNombre() {
+    productos.sort((a, b) => {
+        if (a.nombre > b.nombre) {
+            return 1;
+        }
+        if (a.nombre < b.nombre) {
+            return -1;
+        }
+        return 0;
+    })
+    cargarProductos(productos);
+}
+
+function ordenPrecio() {
+    productos.sort((a, b) => {
+        if (a.precio > b.precio) {
+            return 1;
+        }
+        if (a.precio < b.precio) {
+            return -1;
+        }
+        return 0;
+    })
+    cargarProductos(productos);
+}
+
+function filtroProductos() {
+    inputFiltro.value = inputFiltro.value.toUpperCase();
+    if (inputFiltro.value !== "") {
+        const salida = productos.filter((fil) => fil.nombre.includes(inputFiltro.value));
+            if (salida.length === 0) {
+                console.warn("No se encuentra");
+                cargarProductos(productos);
+            } 
+            else {
+                cargarProductos(salida);
+            }
+    }
+    else {
+        cargarProductos(productos);
+    }
+}
